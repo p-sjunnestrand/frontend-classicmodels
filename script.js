3d4f76e32c0b5ffecf356529c76999dde7f06d99
Petter Sjunnestrand <p.sjunnestrand@gmail.com>
2021-04-28 22:26:21 +0200

    get fetch working

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log('hello world!');
 const navBar = document.getElementById('navBar');
 const navBarUl = document.getElementById('navBarUl');
 const navStart = document.getElementById('navStart');
@@ -6,4 +7,15 @@ const navContact = document.getElementById('navContact');
 
 const mainWrp = document.getElementById('mainWrp');
 
+navProducts.addEventListener('click', () => {
+    console.log('click!');
+    productsPage();
+})
 
+
+function productsPage () {
+    mainWrp.innerHTML = '';
+    fetch('http://localhost:3000/products/')
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
