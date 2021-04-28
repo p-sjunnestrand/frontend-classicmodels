176af890ab56a0eea4b157c0529ed62b73932aef
Petter Sjunnestrand <p.sjunnestrand@gmail.com>
2021-04-28 14:53:29 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const navBar = document.getElementById('navBar');
+const navBarUl = document.getElementById('navBarUl');
+const navStart = document.getElementById('navStart');
+const navProducts = document.getElementById('navProducts');
+const navContact = document.getElementById('navContact');
+
+const mainWrp = document.getElementById('mainWrp');
+
+
