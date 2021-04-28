578247dd92bd4ce329c5a1c4284832a011411bca
Petter Sjunnestrand <p.sjunnestrand@gmail.com>
2021-04-28 23:00:43 +0200

    added productLine menu + clickable items

--- script.js
+++ script.js
@@ -17,5 +17,27 @@ function productsPage () {
     mainWrp.innerHTML = '';
     fetch('http://localhost:3000/products/')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data);
+        for(item in data){
+            const productLineWrp = document.createElement('div');
+            productLineWrp.classList.add('productLineWrp');
+
+            const prodLineId = data[item].productLine.replace(/ /g, '');
+            productLineWrp.id = prodLineId;
+
+            const productLineHeader = document.createElement('h4');
+            productLineHeader.innerHTML = data[item].productLine;
+            productLineHeader.classList.add('productLineHeader');
+
+            const productLineText = document.createElement('article');
+            productLineText.classList.add('productLineText');
+            productLineText.innerHTML = data[item].textDescription;
+
+            productLineWrp.append(productLineHeader, productLineText);
+            mainWrp.appendChild(productLineWrp);
+            // mainWrp.insertAdjacentHTML('beforeend', data[item].productLine)
+        }
+        mainWrp.addEventListener('click', e => console.log(e.target.parentNode));
+    })
 }
